18009e86abe3309fde4bb79ccab1b6c45d1534fa
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-09-29 19:02:34 +0530

    5.3 firebase quering

--- App.js
+++ App.js
@@ -1,22 +1,38 @@
-import  {  intitalizeApp } from 'firebase/app';
-import  {  getFirestore } from 'firebase/firestore'; // Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore'; // Import the functions you need from the SDKs you need
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import the screens
+import ShoppingLists from './components/ShoppingLists';
 
-export default function App() {
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyALAciQB_zxI8PgiZw6YM-J0wY5iE0R4z4",
+    authDomain: "shopping-list-demo-b3785.firebaseapp.com",
+    projectId: "shopping-list-demo-b3785",
+    storageBucket: "shopping-list-demo-b3785.appspot.com",
+    messagingSenderId: "1234567890",
+    appId: "1:1234567890:web:abcdef123456"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="ShoppingLists">
+        <Stack.Screen name="ShoppingLists" component={ShoppingLists} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
